5ac72c74be5d0ab50b5b226f9117b7981c06e785
tired-i <ramen4598@naver.com>
2025-02-03 00:16:05 +0900

    Feat: printf 구현

--- 05_helloworld/run.sh
+++ 05_helloworld/run.sh
@@ -15,7 +15,7 @@ CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
 
 # Start QEMU
 #$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
