2689874b48b44322c82a807cab568a830ce0d545
tired-i <ramen4598@naver.com>
2025-02-02 19:40:23 +0900

    Chore: 챕터 별로 폴더 분리

--- 04_boot/run.sh
+++ 04_boot/run.sh
@@ -3,6 +3,10 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
+# tired-i added
+BASEDIR=$(dirname "$0")
+BIOS_LOCATION=$BASEDIR/../opensbi-riscv32-generic-fw_dynamic.bin
+
 # Path to clang and compiler flags
 # mac Users:
 # CC=/opt/homebrew/opt/llvm/bin/clang  
@@ -14,4 +18,5 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+#$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+$QEMU -machine virt -bios $BIOS_LOCATION -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
